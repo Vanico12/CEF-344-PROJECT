5cb8c061c1a51b9278c1cdcfc502b678d77a2d62
Vanico12 <nkafuvanic760@gmail.com>
2023-05-17 02:23:27 +0100

    First Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import"./Footer.css"
+const Experience = () => {
+    return(
+        <div>Footer</div>
+    )
+}
+
+export default Footer
---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import"./Testimonial.css"
+const Testimonial = () => {
+    return(
+        <div>Testimonail</div>
+    )
+}
+
+export default Testimonial
